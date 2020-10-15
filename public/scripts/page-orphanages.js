33c3979769d9cf4b75add8727432e883e62e612c
AnandaCba <fabiogurudeva@gmail.com>
2020-10-15 01:12:13 -0300

    Create a new window and add a map

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,29 @@
+//create map
+const map = L.map('mapid').setView([-23.3382024,-46.8437658], 15);
+
+//create and add TileLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    //create icon
+    const icon = L.icon({
+        iconUrl:"./public/images/map-marker.svg",
+        iconSize:[58,68],
+        iconAnchor:[29,68],
+        popupAnchor:[170,2]
+
+    })
+
+    //create popup overlay
+    const popup = L.popup({
+        closeButton:false,
+        className:'map-popup',
+        minWidth:240,
+        minHeight:240
+    }).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+//create and add TileLayer
+L   
+.marker([-23.3382024,-46.8437658],{icon})
+.addTo(map)
+.bindPopup(popup);
